5e806b89ed8e19a1546edef206238a4340c955f5
Valerio Marcelli <marcelli@seattleu.edu>
2019-06-03 14:31:23 -0700

    update generator with sha1 test

--- pagan-master/test/test_generator.py
+++ pagan-master/test/test_generator.py
@@ -1,45 +1,15 @@
-'''
-  _    _ _   _ _      ______  _____ _____  __     ______  _    _ _____    _   _          __  __ ______    
- | |  | | \ | | |    |  ____|/ ____/ ____| \ \   / / __ \| |  | |  __ \  | \ | |   /\   |  \/  |  ____|   
- | |  | |  \| | |    | |__  | (___| (___    \ \_/ / |  | | |  | | |__) | |  \| |  /  \  | \  / | |__      
- | |  | | . ` | |    |  __|  \___ \\___ \    \   /| |  | | |  | |  _  /  | . ` | / /\ \ | |\/| |  __|     
- | |__| | |\  | |____| |____ ____) |___) |    | | | |__| | |__| | | \ \  | |\  |/ ____ \| |  | | |____    
-  \____/|_|_\_|______|______|_____/_____/___ _|_|_ \____/ \____/|_|  \_\ |_| \_/_/    \_\_|  |_|______|   
- |_   _|/ ____| \ \    / /\   | |    |  ____|  __ \|_   _/ __ \                                           
-   | | | (___    \ \  / /  \  | |    | |__  | |__) | | || |  | |                                          
-   | |  \___ \    \ \/ / /\ \ | |    |  __| |  _  /  | || |  | |                                          
-  _| |_ ____) |    \  / ____ \| |____| |____| | \ \ _| || |__| |                                          
- |_____|_____/    _ \/_/ ___\_\______|______|_| _\_\_____\____/____ _    _   _______ _    _ _____  _____  
- |  __ \ / __ \  | \ | |/ __ \__   __| |__   __/ __ \| |  | |/ ____| |  | | |__   __| |  | |_   _|/ ____| 
- | |  | | |  | | |  \| | |  | | | |       | | | |  | | |  | | |    | |__| |    | |  | |__| | | | | (___   
- | |  | | |  | | | . ` | |  | | | |       | | | |  | | |  | | |    |  __  |    | |  |  __  | | |  \___ \  
- | |__| | |__| | | |\  | |__| | | |       | | | |__| | |__| | |____| |  | |    | |  | |  | |_| |_ ____) | 
- |_____/ \____/  |_|_\_|\____/__|_|   _  _|_|_ \____/_\____/ \_____|_|__|_|    |_|  |_|  |_|_____|_____/  
- |  ____| |  | |/ ____| |/ /_   _| \ | |/ ____|  / ____/ __ \|  __ \|  ____|                              
- | |__  | |  | | |    | ' /  | | |  \| | |  __  | |   | |  | | |  | | |__                                 
- |  __| | |  | | |    |  <   | | | . ` | | |_ | | |   | |  | | |  | |  __|                                
- | |    | |__| | |____| . \ _| |_| |\  | |__| | | |___| |__| | |__| | |____                               
- |_|     \____/ \_____|_|\_\_____|_| \_|\_____|  \_____\____/|_____/|______|                              
- 
+'''                              
+ UNLESS YOUR NAME IS VALERIO DO NOT TOUCH THIS CODE
 '''
 import pagan
 import pytest
 import sys
 import os
+import hashlib
 
-def generator_MD5():
-    print()
 
-def test_hashGen():
-    import hashlib
+def generator_MD5(input):
     md5 = hashlib.md5()
-    input = ""
-
-    #Check for version issues
-    if(sys.version_info.major == 2):
-        input = bytes(input)
-    else:
-        input = bytes(input, "utf-8")
 
     #Update the Hashcode with the input
     #Generate the image using hash
@@ -55,5 +25,26 @@ def test_hashGen():
     with pytest.raises(pagan.generator.FalseHashError):
         imgOne = pagan.generator.generate_by_hash(md5.hexdigest()+"A")
 
-    #Test Globals
-    assert pagan.generator.OUTPUT_PATH == ('%s%soutput%s' % (os.getcwd(), os.sep, os.sep))
+def generator_SHA1(input):
+    sha1 = hashlib.sha1()
+
+    #Update the Hashcode with the input
+    #Generate the image using hash
+    sha1.update(input)
+    imgOne = pagan.generator.generate_by_hash(sha1.hexdigest())
+    imgTwo = pagan.generator.generate("", 1) #Default generator is md5
+    assert (imgOne is not None)
+    assert (imgOne == imgTwo)
+
+def test_hashGen():
+    input = ""
+    #Check for version issues
+    if(sys.version_info.major == 2):
+        input = bytes(input)
+    else:
+        input = bytes(input, "utf-8")
+
+    generator_MD5(input)
+    generator_SHA1(input)
+
+    
