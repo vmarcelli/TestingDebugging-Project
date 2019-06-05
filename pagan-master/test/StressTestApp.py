9d02376a50f07fa6e166e743271aa210185b8006
Valerio Marcelli <marcelli@seattleu.edu>
2019-06-05 16:26:25 -0700

    changes to program

--- pagan-master/test/StressTestApp.py
+++ pagan-master/test/StressTestApp.py
@@ -1,3 +1,7 @@
+'''
+Runs the test suite in parallel x number of times
+
+'''
 import os
 import sys
 import datetime
@@ -5,14 +9,14 @@ import datetime
 #Get the arguments from CLI
 paramIn = sys.argv[1]
 numofTimes = int(paramIn)
-command = "python runTestSuite.py 10"
+command = "python runTestSuite.py 10 &"
 
 timeStart = datetime.datetime.now()
 for num in range(0, numofTimes):   
     os.system(command)
 timeEnd = datetime.datetime.now()
 
-print("Input parameters used for StressTest:", numofTimes)
+print("Input parallels used for StressTest:", numofTimes)
 print("Number of times runTestSuite was used: 10")
 print("Time started:", timeStart)
 print("Time ended:", timeEnd)
