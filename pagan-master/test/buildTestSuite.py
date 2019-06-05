a9d5a5bfe7cd631a79c927508850d593bf39f674
Valerio Marcelli <marcelli@seattleu.edu>
2019-06-05 16:18:03 -0700

    instructions for building the test suite

--- pagan-master/test/buildTestSuite.py
+++ pagan-master/test/buildTestSuite.py
@@ -1,5 +1,15 @@
-import os
+'''
+Please run this build script file from inside of the test folder in
+pagan-master/test
 
+It should set up the pagan dependencies and install pytest for you
+ONLY RUN THIS FROM AN ANACONDA 3 SHELL 
+If you need instructions on how to use Anaconda see the README.md provided
+to get set up
+
+'''
+
+import os
 os.chdir(path="../")
-os.system("python install setup.py")
+os.system("python setup.py install")
 os.system("conda install pytest")
